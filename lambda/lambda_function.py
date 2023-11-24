26e6738fb21251943f4a3d7d93e70a81afcaa7a8
brycegoh <brycegoh96@gmail.com>
2023-11-25 03:04:28 +0800

    feat: added lambda

---
+++ lambda/lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import numpy as np
+import os
+from supabase import create_client
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+db = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def lambda_handler(event, context):
+    temp = event.get('temp')
+    humidity = event.get('humidity')
+    flame = event.get('flame')
+    air_quality = event.get('airQuality')
+    array1 = np.array([1, 2, 3, 4, 5]);
+    array2 = np.array([5, 4, 3, 2, 1]);
+    coef = np.corrcoef(array1, array2)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'probability': 0.5,
+            'temp': temp,
+            'humidity': humidity,
+            'flame': flame,
+            'airQuality': air_quality,
+            'coef': [
+                coef[0][1], coef[1][0],
+            ],
+        })
+    }
