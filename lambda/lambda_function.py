a80ae2438ae4625687332b84ae45a2380b575996
brycegoh <brycegoh96@gmail.com>
2023-11-25 03:17:41 +0800

    feat: add test supabase

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,7 +5,7 @@ from supabase import create_client
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-db = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def lambda_handler(event, context):
     temp = event.get('temp')
@@ -15,6 +15,7 @@ def lambda_handler(event, context):
     array1 = np.array([1, 2, 3, 4, 5]);
     array2 = np.array([5, 4, 3, 2, 1]);
     coef = np.corrcoef(array1, array2)
+    res = supabase.from_('firecloud').__eq__("id",552).execute()
     return {
         'statusCode': 200,
         'body': json.dumps({
@@ -26,5 +27,6 @@ def lambda_handler(event, context):
             'coef': [
                 coef[0][1], coef[1][0],
             ],
+            'data': res.get('data')
         })
     }
